d29a5674958a09c20e5b9dad0940dd566306604b
sprv1tp1-jpg <sprv1tp1@gmail.com>
2025-09-14 21:53:22 +0900

    Add files via upload

---
+++ web.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Discord Bot is running!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
